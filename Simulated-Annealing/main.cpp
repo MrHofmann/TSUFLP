e09577bc1bc4e563230323e212cb78e113b6b3b4
MrHofmann <mdfkprekid@hotmail.com>
2016-09-23 14:04:23 +0200

    23-09-2016

---
+++ Simulated-Annealing/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "sa.h"
+
+
+unsigned temp = 50000;
+double alpha = 0.99;
+Solution global;
+
+//------------------------------------------------------------------------------------//
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2)
+    {
+        cerr << "Enter instance path!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    srand(time(0));
+
+    InputData data;
+    SA_Load(argv[1], data);
+
+    int t1 = clock();
+    int t2;
+    int t3 = t1;
+//    int t4;
+//    int tmax=0;
+    SA_InitSolution(global, data._J, data._K);
+    SA_SortMatrix(data._c, data._d, data._vm);
+
+    unsigned iterations = data._J + data._K;
+    double t = temp;
+    for(unsigned k=0; t>0.01 && k<100; k++)
+    {
+        double tmp = SA_Evaluate(data, global);
+        Solution s;
+        SA_InitSolution(s, data._J, data._K);
+
+        for(unsigned i=0; i<iterations*10; i++)
+        {
+            Solution sp;
+            double rnd = SA_GetRandomUniform1(0.0, 1.0);
+
+            if(rnd < 0.75)
+                sp = SA_GetRandomNeighbor1(s);
+            else
+            {
+                sp = SA_GetRandomNeighbor1(s);
+                SA_Compute(data, t, s, sp, global);
+
+                sp = SA_GetRandomNeighbor2(s);
+            }
+        }
+
+        SA_ApplyGeometricCooling(t);
+
+        if(SA_Evaluate(data, global) < tmp)
+        {
+//            t4 = t3;
+            t3 = clock();
+            k=0;
+
+//            if(t3-t4 > tmax)
+//                tmax = t3-t4;
+        }
+    }
+
+    t2 = clock();
+    double tmp = SA_Evaluate(data, global);
+    SA_Save(argv[1], global, tmp, t1, t2, t3);
+//    cout << (double)(t3-t1)/CLOCKS_PER_SEC << endl;
+//    cout << (double)tmax/CLOCKS_PER_SEC << endl;
+
+    return 0;
+}
---
+++ Simulated-Annealing/sa.cpp
@@ -0,0 +1,350 @@
+#include <limits>
+#include <random>
+#include "sa.h"
+
+void SA_Load(const string &s, InputData &D)
+{
+    string str;
+    str = "instances/input_" + s + ".txt";
+
+    ifstream file(str);
+    if(!file.is_open())
+    {
+        cerr << "Error open file" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    vector<string> data;
+    string line;
+
+
+    getline(file, line);
+    {
+        istringstream iss(line);
+        copy(istream_iterator<string>(iss),
+             istream_iterator<string>(),
+             back_inserter(data));
+
+        D._I = stoul(data[0].c_str());
+        D._J = stoul(data[1].c_str());
+        D._K = stoul(data[2].c_str());
+    }
+
+
+    getline(file, line);
+    getline(file, line);
+
+
+    getline(file, line);
+    {
+        data.clear();
+        istringstream iss(line);
+        copy(istream_iterator<string>(iss),
+             istream_iterator<string>(),
+             back_inserter(data));
+    }
+    for(unsigned i=0; i<data.size(); i++)
+        D._D.push_back(stoul(data[i]));
+
+
+    getline(file, line);
+    {
+        data.clear();
+        istringstream iss(line);
+        copy(istream_iterator<string>(iss),
+             istream_iterator<string>(),
+             back_inserter(data));
+    }
+    for(unsigned i=0; i<data.size(); i++)
+        D._f.push_back(stoul(data[i]));
+
+
+    getline(file, line);
+    {
+        data.clear();
+        istringstream iss(line);
+        copy(istream_iterator<string>(iss),
+             istream_iterator<string>(),
+             back_inserter(data));
+    }
+    for(unsigned i=0; i<data.size(); i++)
+        D._g.push_back(stoul(data[i]));
+
+
+    getline(file, line);
+    getline(file, line);
+
+
+    for(unsigned i=0; i<D._I; i++)
+    {
+        getline(file, line);
+
+        data.clear();
+        istringstream iss(line);
+        copy(istream_iterator<string>(iss),
+             istream_iterator<string>(),
+             back_inserter(data));
+
+        vector<double> v;
+        for(unsigned j=0; j<D._J; j++)
+             v.push_back(stod(data[j]));
+
+        D._c.push_back(v);
+    }
+
+
+    getline(file, line);
+    getline(file, line);
+
+
+    for(unsigned j=0; j<D._J; j++)
+    {
+        getline(file, line);
+
+        data.clear();
+        istringstream iss(line);
+        copy(istream_iterator<string>(iss),
+             istream_iterator<string>(),
+             back_inserter(data));
+
+        vector<double> v;
+        for(unsigned k=0; k<D._K; k++)
+            v.push_back(stod(data[k]));
+
+        D._d.push_back(v);
+    }
+
+    file.close();
+
+}
+
+void SA_Save(const string s, Solution &g, double &v, int t1, int t2, int t3)
+{
+    string str;
+    str = "results/output_" + s + ".txt";
+
+    ofstream file(str, ios::app);
+    if(!file.is_open())
+    {
+        cerr << "Output failed!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    stringstream ss;
+    time_t t = time(0);
+    struct tm * now = localtime( & t );
+    ss <<  now->tm_mday << '-'
+        << (now->tm_mon + 1) << '-'
+        << (now->tm_year + 1900) << " "
+        << now->tm_hour << ":"
+        << now->tm_min << ":"
+        << now->tm_sec << endl;
+
+
+    vector<bool> y = g.GetY();
+    unsigned depots = 0;
+    for(unsigned j=0; j<y.size(); j++)
+        if(y[j])
+            depots++;
+
+    vector<bool> z = g.GetZ();
+    unsigned plants = 0;
+    for(unsigned k=0; k<z.size(); k++)
+        if(z[k])
+            plants++;
+
+
+    file << "INSTANCE " << s << ":\t" << ss.str() << endl
+         << "Plants:\t\t" << plants << endl
+         << "Depots:\t\t" << depots << endl
+         << "Function Value:\t" << v << endl
+         << "Time:\t\t" << double(t2-t1)/CLOCKS_PER_SEC << endl
+         << "Last Update:\t" << double(t3-t1)/CLOCKS_PER_SEC << endl << endl << endl;
+
+    file.close();
+}
+
+void SA_InitSolution(Solution &s,unsigned J, unsigned K)
+{
+    //RANDOMIZE Y
+    vector<bool> y;
+    vector<unsigned> y_ind;
+    while(y_ind.empty())
+    {
+        y.clear();
+        y_ind.clear();
+        for(unsigned j=0; j<J; j++)
+        {
+            unsigned s = rand()%2;
+            y.push_back(s);
+
+            if(s == 1)
+                y_ind.push_back(j);
+        }
+    }
+
+
+    //RANDOMIZE Z
+    vector<bool> z;
+    vector<unsigned> z_ind;
+    while(z_ind.empty())
+    {
+        z.clear();
+        z_ind.clear();
+        for(unsigned k=0; k<K; k++)
+        {
+            unsigned s = rand()%2;
+            z.push_back(s);
+
+            if(s == 1)
+                z_ind.push_back(k);
+        }
+    }
+
+    s = Solution(y, z);
+}
+
+void SA_SortMatrix(const vector<vector<double> > &c, const vector<vector<double> > &d,
+                         vector<multimap<double, key> > &vm)
+{
+    for(unsigned i=0; i<c.size(); i++)
+    {
+        multimap<double, key> m;
+        for(unsigned j=0; j<d.size(); j++)
+        {
+            for(unsigned k=0; k<d[j].size(); k++)
+            {
+                double s = c[i][j] + d[j][k];
+                m.insert(make_pair(s, key(j, k)));
+            }
+        }
+
+        vm.push_back(m);
+    }
+}
+
+
+Solution SA_GetRandomNeighbor1(const Solution &s)
+{
+    vector<bool> y = s.GetY();
+    vector<bool> z = s.GetZ();
+
+    bool b = true;
+    while(b)
+    {
+        y = s.GetY();
+
+        unsigned r = rand()%y.size();
+        y[r] = !y[r];
+
+        for(unsigned j=0; j<y.size(); j++)
+            if(y[j])
+                b = false;
+    }
+
+    return Solution(y,z);
+}
+
+Solution SA_GetRandomNeighbor2(const Solution &s)
+{
+    vector<bool> y = s.GetY();
+    vector<bool> z = s.GetZ();
+
+    bool b = true;
+    while(b)
+    {
+        z = s.GetZ();
+
+        unsigned r = rand()%z.size();
+        z[r] = !z[r];
+
+        for(unsigned k=0; k<z.size(); k++)
+            if(z[k])
+                b = false;
+    }
+
+    return Solution(y,z);
+}
+
+
+double SA_Evaluate(const InputData &data, const Solution &s)
+{
+    vector<bool> y = s.GetY();
+    vector<bool> z = s.GetZ();
+
+    int sum1 = 0;
+    for(unsigned j=0; j<data._J; j++)
+        sum1 += data._f[j]*y[j];
+
+    if(sum1 == 0)
+        return numeric_limits<double>::max();
+
+    int sum2 = 0;
+    for(unsigned k=0; k<data._K; k++)
+        sum2 += data._g[k]*z[k];
+
+    if(sum2 == 0)
+        return numeric_limits<double>::max();
+
+
+    double sum3 = 0;
+    for(unsigned i=0; i<data._I; i++)
+    {
+        double tmp1 = numeric_limits<double>::max();
+
+        for(unsigned j=0; j<y.size(); j++)
+            for(unsigned k=0; k<z.size(); k++)
+                if(y[j] && z[k])
+                {
+                    double tmp2 = data._D[i]*(data._c[i][j] + data._d[j][k]);
+                    if(tmp2 < tmp1)
+                        tmp1 = tmp2;
+                }
+
+        sum3 += tmp1;
+    }
+
+    return sum1+sum2+sum3;
+}
+
+void SA_Compute(InputData &data, double t, Solution &s, Solution &sp, Solution &global)
+{
+    double tmp = SA_Evaluate(data, sp);
+    if(tmp < SA_Evaluate(data, global))
+    {
+        global = sp;
+        cout << "New global: " << tmp << endl;
+        global.PrintSolution();
+    }
+    if(tmp < SA_Evaluate(data, s))
+        s = sp;
+    else
+    {
+        double delta_f = tmp - SA_Evaluate(data, s);
+        double p = SA_GetRandomUniform1(0.0, 1.0);
+
+        if(p > 1/exp(delta_f/t))
+            s = sp;
+    }
+}
+
+
+void SA_ApplyGeometricCooling(double &t)
+{
+    t *= alpha;
+}
+
+double SA_GetRandomUniform1(double left, double right)
+{
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_real_distribution<double> distribution(left, right);
+
+    return distribution(generator);
+}
+
+double SA_GetRandomUniform2(double left, double right)
+{
+    double rnd = (double)rand()/RAND_MAX;
+    return rnd*(right - left) + left;
+}
